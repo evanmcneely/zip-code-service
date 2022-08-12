5da0be5e8d54d6bf52fa338ea0ade97a710539db
[Evan McNeely] <evanmcneely@me.com>
2022-08-12 15:13:20 -0700

    copleted ititial setup of ZipCode search result

--- src/components/ZipCode.tsx
+++ src/components/ZipCode.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import { ZipCodeProps } from "../../@types/AppTypes";
 
 export default function ZipCode({searchResult}:ZipCodeProps) {
-    return <div>Zipcode</div>
+    const {countryCode, city, state, zipCode} = searchResult;
+    const codeName = countryCode.toLocaleLowerCase() === 'us' ? 'ZipCode': 'Postalcode';
+    return (
+    <div className="zipCodeItem">
+        <p>{`The ${codeName} ${zipCode} is located in ${city}, ${state}`}</p>
+    </div>
+    )
 }
