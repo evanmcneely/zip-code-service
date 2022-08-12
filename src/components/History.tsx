e2fbf1d90c0885d201220cb9582316c7e57ea946
[Evan McNeely] <evanmcneely@me.com>
2022-08-12 15:06:48 -0700

    completed initial setup of history component

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { HistoryProps } from "../../@types/AppTypes";
+import ZipCode from "./ZipCode";
 
 export default function History({searchHistory}:HistoryProps) {
-    return <div>History</div>
-}
+    return (
+    <>
+        <div id="historyHeader">
+            <div>Search History</div>
+            <button id="clearButton">Clear</button>
+        </div>
+        <div>
+            {searchHistory.map((search) => {
+                return <ZipCode searchResult={search} />;
+            })}
+        </div>
+    </>
+    )
+};
